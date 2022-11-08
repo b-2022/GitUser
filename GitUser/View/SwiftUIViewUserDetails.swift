21b8d510fb1b88f67a2b9ce904bc75747766267f
Kng <kng14169@hotmail.com>
2022-11-08 08:19:29 +0800

    Fix tableview image loading issue Fixed core data saving issue

---
+++ GitUser/View/SwiftUIViewUserDetails.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewUserDetails.swift
+//  GitUser
+//
+//  Created by Boon on 07/11/2022.
+//
+
+import SwiftUI
+
+struct SwiftUIViewUserDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIViewUserDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewUserDetails()
+    }
+}
